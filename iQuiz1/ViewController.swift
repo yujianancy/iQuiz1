446d077100f99603071b25b51e342dd7b75a28d4
Jia Yu <yujia1@D-172-28-35-200.dhcp4.washington.edu>
2015-11-03 14:21:49 -0800

    Initial Commit

---
+++ iQuiz1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iQuiz1
+//
+//  Created by Jia Yu on 11/3/15.
+//  Copyright (c) 2015 Jia Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
